f641d01e93d716d76547bfb7d8b0191ee205a597
Bauyrzhan <billfookingmurray@gmail.com>
2024-12-27 16:58:07 +0500

    Homework 8

--- hws/hw1/ex21.py
+++ hws/hw1/ex21.py
@@ -1,10 +1,3 @@
-# Exercise 21: exponentiation
-#
-# Declare a palindrome string (A palindrome is a word that is spelled the same forward and backward. ex: "racecar")
-# Check if it is palindromic without using loops
+def unik(my_list: list) -> int:
+    return list(set(my_list))
 
-def solution(word):
-    return word == word[::-1]
-
-
-print(solution('car'))
--- hws/hw1/exx.py
+++ hws/hw1/exx.py
@@ -1,1 +1,5 @@
-words = map(str(input()))
+def find_index(my_list: list, element: int) -> int:
+    if element in my_list:
+        return my_list.index(element)
+    else:
+        return -1
