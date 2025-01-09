a11b539b92c505da387a0ce1f1ba6cf583293594
enkelii6 <kukarsevgleb@gmail.com>
2025-01-09 20:35:44 +0500

    argparse

--- projects/ascii_art/main.py
+++ projects/ascii_art/main.py
@@ -8,6 +8,7 @@ def main(word):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('word', type=str)
+    parser.add_argument('-r', '--reverse', type=str, default='')
     args = parser.parse_args()
 
     main(args.word)
