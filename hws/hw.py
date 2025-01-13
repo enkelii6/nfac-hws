fe9989083e54561a6300a438758907c68a965999
Niyaz <Niyaz>
2025-01-13 14:43:22 +0500

    update

--- hws/hw.py
+++ hws/hw.py
@@ -1,4 +1,5 @@
 from collections import Counter
+
 """
 Exercise-1: Find missing elements
 Write a function "missing_elements(my_list: list) -> list" that takes a
@@ -7,6 +8,7 @@ sorted list of integers and returns a list of missing integers in the range of t
 Example:
 missing_elements([1, 2, 4, 6, 7]) -> [3, 5]
 """
+
 
 def missing_elements(my_list: list) -> list:
     if not my_list:
@@ -17,7 +19,6 @@ def missing_elements(my_list: list) -> list:
     full_range = set(range(min_val, max_val+1))
     my_set = set(my_list)
     missing = list(full_range-my_set)
-
     return missing
 
 
@@ -30,6 +31,7 @@ from the list and values are their counts in the list.
 Example:
 count_occurrences([1, 2, 3, 1, 2, 4, 5, 4]) -> {1: 2, 2: 2, 3: 1, 4: 2, 5: 1}
 """
+
 
 def count_occurrences(my_list: list) -> dict:
     count_occur = {}
@@ -50,6 +52,7 @@ Example:
 common_elements([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]) -> [3, 4, 5]
 """
 
+
 def common_elements(list1: list, list2: list) -> list:
     uniq_el = list(set(list1) & set(list2))
     return uniq_el
@@ -64,6 +67,7 @@ Example:
 char_frequency('hello world') -> {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
 """
 
+
 def char_frequency(my_string: str) -> dict:
     frequency = Counter(my_string)
     return dict(frequency)
@@ -77,6 +81,7 @@ string and returns the number of unique words in the string.
 Example:
 unique_words('hello world hello') -> 2
 """
+
 
 def unique_words(my_string: str) -> int:
     words = my_string.split()
@@ -93,15 +98,16 @@ Example:
 word_frequency('hello world hello') -> {'hello': 2, 'world': 1}
 """
 
+
 def word_frequency(my_string: str) -> dict:
     words = my_string.split()
-    frequency_dict = {}
+    defaultdict = {}
     for word in words:
-        if word in frequency_dict:
-            frequency_dict[word] +=1
+        if word in defaultdict:
+            defaultdict[word] +=1
         else:
-            frequency_dict[word] = 1
-    return dict(frequency_dict)
+            defaultdict[word] = 1
+    return defaultdict
 
 
 """
@@ -114,10 +120,10 @@ Example:
 count_in_range([1, 2, 3, 4, 5, 4, 3, 2, 1], 2, 4) -> 3
 """
 
+
 def count_in_range(my_list: list, start: int, end: int) -> int:
     uniq_elem = {i for i in my_list if start <= i <=end}
     return len(uniq_elem)
-
 
 
 """
@@ -130,6 +136,7 @@ Example:
 swap_dict({1: 'a', 2: 'b', 3: 'c'}) -> {'a': 1, 'b': 2, 'c': 3}
 """
 
+
 def swap_dict(d: dict) -> dict:
     new_dict = {}
     for key, value in d.items():
@@ -137,6 +144,7 @@ def swap_dict(d: dict) -> dict:
             new_dict[value] = key
 
     return new_dict
+
 
 """
 Exercise-10: Subset check
@@ -146,6 +154,7 @@ sets and returns True if set2 is a subset of set1, and False otherwise.
 Example:
 is_subset({1, 2, 3, 4, 5}, {3, 4, 5}) -> True
 """
+
 
 def is_subset(set1: set, set2: set) -> bool:
     return set2.issubset(set1)
@@ -160,9 +169,11 @@ Example:
 list_intersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]) -> [3, 4, 5]
 """
 
+
 def list_intersection(list1: list, list2: list) -> list:
     intersection = set(list1).intersection(set(list2))
     return list(intersection)
+
 
 """
 Exercise-12: Union of lists
@@ -172,6 +183,7 @@ lists and returns a list of unique elements that are in either of the lists.
 Example:
 list_union([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]) -> [1, 2, 3, 4, 5, 6, 7]
 """
+
 
 def list_union(list1: list, list2: list) -> list:
     united_list = set(list1).union(set(list2))
@@ -187,12 +199,14 @@ Example:
 most_frequent([1, 2, 3, 1, 2, 4, 5, 4, 1]) -> 1
 """
 
+
 def most_frequent(my_list: list) -> int:
     if not my_list:
         return None
     count = Counter(my_list)
     most_freq_el = count.most_common(1)[0][0]
     return most_freq_el
+
 
 """
 Exercise-14: Least frequent element
@@ -202,6 +216,7 @@ list of integers and returns the least frequent element in the list.
 Example:
 least_frequent([1, 2, 3, 1, 2, 4, 5, 4, 1]) -> 3
 """
+
 
 def least_frequent(my_list: list) -> int:
     if not my_list:
