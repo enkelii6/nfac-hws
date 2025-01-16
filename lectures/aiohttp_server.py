5e5a510b3c7767e69d3102bd8c8aed8260ed6607
enkelii6 <kukarsevgleb@gmail.com>
2025-01-16 20:08:22 +0500

    lecture

--- lectures/aiohttp_server.py
+++ lectures/aiohttp_server.py
@@ -2,16 +2,37 @@ from aiohttp import web
 
 books = [
     {
-        'id': '1',
+        'id': 1,
         'title': '1984',
         'description': 'Book that changed the world',
         'rating': '4.99',
     },
+    {
+        'id': 2,
+        'title': 'Fight Club',
+        'description': 'Man with schizofrenia trying to socialize',
+        'rating': '1.21',
+    }
 ]
 
 
 async def handle(request):
-    return web.json_response({'hello': 'world'})
+    params_json = dict(request.rel_url.query)
+
+    return web.json_response(await filter_books(books, **params_json))
+
+
+async def filter_books(books, **filters):
+    filtered_books = []
+    for book in books:
+        for key, value in filters.items():
+            try:
+                if book[key] == value:
+                    filtered_books.append(book)
+            except KeyError:
+                continue
+
+    return filtered_books
 
 
 app = web.Application()
--- lectures/async_prog.py
+++ lectures/async_prog.py
@@ -5,9 +5,9 @@ import aiohttp
 
 async def get_google():
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://0.0.0.0:8080') as response:
+        async with session.get('http://0.0.0.0:8080', params={'example': 'Fight Club'}) as response:
             response_json = await response.json()
-            print(response_json['hello'])
+            print(response_json)
 
 
 asyncio.run(get_google())
