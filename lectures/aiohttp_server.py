d9c6e27f64e2c22ee920143234fc3e249e1c82e6
enkelii6 <kukarsevgleb@gmail.com>
2025-01-21 20:31:36 +0500

    lectures

--- lectures/aiohttp_server.py
+++ lectures/aiohttp_server.py
@@ -17,9 +17,25 @@ books = [
 
 
 async def handle(request):
-    params_json = dict(request.rel_url.query)
+    params_json = dict(request.query)
+    # /?id=1&page=0 -> {'id': 1, 'page': 0}
+    page = int(params_json.pop('page', 0))
+    limit = int(params_json.pop('limit', 10))
+    sort = params_json.pop('sort', 'rating')
 
-    return web.json_response(await filter_books(books, **params_json))
+    res = copy.deepcopy(books)
+    if sort:
+        res = await sort_books(res, sort)
+    if params_json:
+        res = await filter_books(res, **params_json)
+    if page and limit:
+        res = await paginate_books(res, page, limit)
+
+    return web.json_response(res)
+
+
+async def sort_books(books, sort_param):
+    return books.sort(key = lambda book: book[sort_param])
 
 
 async def filter_books(books, **filters):
@@ -35,8 +51,16 @@ async def filter_books(books, **filters):
     return filtered_books
 
 
+async def paginate_books(books, page = 0, limit = 10):
+    first = limit * page
+    last = first + limit
+
+    return books[first:last]
+
+
 app = web.Application()
 app.add_routes([web.get('/', handle)])
+# 127.0.0.1:8080/?page=0&limit=1&title=1984
 
 
 if __name__ == '__main__':
--- lectures/async_prog.py
+++ lectures/async_prog.py
@@ -5,7 +5,7 @@ import aiohttp
 
 async def get_google():
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://0.0.0.0:8080', params={'example': 'Fight Club'}) as response:
+        async with session.get('http://0.0.0.0:8080', params={'example': 'Fight Club', 'sort': 'Year'}) as response:
             response_json = await response.json()
             print(response_json)
 
