bfadebdea44b6dc7e40c8ecbb235fd040ce80791
Niyaz <Niyaz>
2025-01-26 18:53:39 +0500

    update

--- New_project/read_file.py
+++
@@ -1,5 +0,0 @@
-file_name = 'example.txt'
-
-with open(file_name, 'r') as file:
-    for line in file:
-        print(line.strip())
--- New_project/write_file.py
+++
@@ -1,7 +0,0 @@
-file_name = 'example.txt'
-
-with open(file_name, 'w') as file:
-    file.write("Hello!\n")
-    file.write("My Name is Niyaz.\n")
-    file.write("I'am a Nfactorial student")
-
